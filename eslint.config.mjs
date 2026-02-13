4e94561e1a72a4388a201012ea47970a9d648230
promehedi <admin@promehedi.com>
2026-02-13 17:09:26 +0600

    Configure eslint and husky to consistent code format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from '@antfu/eslint-config'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default antfu({
+  type: 'app',
+  nextjs: true,
+  node: {
+    overrides: {
+      'node/no-process-env': 'error',
+      'node/prefer-global/process': 'off'
+    }
+  },
+  typescript: {
+    tsconfigPath: './tsconfig.json'
+  },
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: false,
+    quotes: 'single'
+  },
+  ignores: ['**/migrations/*', '*.md', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'components/ui/**']
+}, {
+  rules: {
+    'no-console': ['warn'],
+    'style/brace-style': ['warn', '1tbs'],
+    'style/comma-dangle': ['warn', 'never'],
+    'antfu/no-top-level-await': ['off'],
+    'style/no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxEOF: 0
+    }],
+    'perfectionist/sort-imports': ['error', {
+      tsconfig: {
+        rootDir: '.'
+      }
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: [/\.md$/]
+    }]
+  }
+})
