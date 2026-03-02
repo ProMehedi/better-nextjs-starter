c84ebe186d85b43fe73092777cfc08d2c94d8d86
promehedi <admin@promehedi.com>
2026-03-02 23:21:16 +0600

    chore(eslint): Update ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,6 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   type: 'app',
   nextjs: true,
-  node: {
-    overrides: {
-      'node/no-process-env': 'error',
-      'node/prefer-global/process': 'off'
-    }
-  },
   typescript: {
     tsconfigPath: './tsconfig.json'
   },
@@ -18,14 +12,27 @@ export default antfu({
     semi: false,
     quotes: 'single'
   },
-  ignores: ['**/migrations/*', '*.md', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'src/components/ui/**']
+  ignores: ['**/migrations/*', '**/*.md', '.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'src/components/ui/**']
 }, {
   rules: {
+    'next/no-img-element': ['off'],
     'no-console': ['warn'],
     'style/eol-last': 'off',
     'style/brace-style': ['warn', '1tbs'],
     'style/comma-dangle': ['warn', 'never'],
     'antfu/no-top-level-await': ['off'],
+    'ts/strict-boolean-expressions': ['off'],
+    'ts/no-floating-promises': ['off'],
+    'ts/no-unsafe-member-access': ['off'],
+    'ts/no-unsafe-call': ['off'],
+    'ts/no-unsafe-argument': ['off'],
+    'ts/no-unsafe-assignment': ['off'],
+    'ts/no-unsafe-return': ['off'],
+    'ts/no-misused-promises': ['off'],
+    'ts/unbound-method': ['off'],
+    'ts/no-use-before-define': ['off'],
+    'node/no-process-env': ['off'],
+    'node/prefer-global/process': ['off'],
     'style/no-multiple-empty-lines': ['error', {
       max: 1,
       maxEOF: 0
